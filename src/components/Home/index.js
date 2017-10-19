f7ef7d637ee34ebe94029c3a0282c06105d2f8e0
Dang Min <stea.steven@gmail.com>
2017-10-19 11:41:38 +0300

    Add Features and Article.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react'
+import {isEmpty, chunk} from 'lodash'
+
+import Loading from '../Misc/Loading'
 
 import {HOME, CORS_PROXY, FETCH_HDS as HEADER} from '../../config/api'
 import LeaderBoard from './LeaderBoard'
@@ -11,7 +14,8 @@ class Home extends Component {
     this.state = {
       dailyLeaders: {},
       seasonRecap: {},
-      seasonLeaders: {}
+      seasonLeaders: {},
+      editorial: {}
     }
   }
 
@@ -32,10 +36,17 @@ class Home extends Component {
     .then(response => response.json())
     .then(response => this.setState({seasonLeaders: response}))
     .catch(console.log("Unable to get season leaders."))
+
+    fetch(`${CORS_PROXY}${HOME.EDITORIAL}`, {headers: HEADER})
+    .then(response => response.json())
+    .then(response => this.setState({editorial: response}))
+    .catch(console.log("Unable to get editorial."))
   }
 
   render() {
-    const {dailyLeaders, seasonRecap, seasonLeaders} = this.state
+    const {dailyLeaders, seasonRecap, seasonLeaders, editorial} = this.state
+    const [beyondNumbers, tidbit, spotlight, fantasyNews, assistTracker, shotchart, hpLink] = !isEmpty(editorial) ? editorial.items : Array(7)
+
     return (
       <div className='content'>
         <h1>Stats Home</h1>
@@ -44,7 +55,11 @@ class Home extends Component {
           <div className='sub-content' style={{flex:4}}>
             <LeaderBoard leaders={dailyLeaders}/>
             <hr />
+            <Features content={hpLink} />
+            <hr />
             <LeaderBoard leaders={seasonLeaders}/>
+            <hr />
+            <Article content={beyondNumbers} />
           </div>
           <div className='subSidebar' style={{flex:1}}>
             <Recap content={seasonRecap} />
@@ -53,6 +68,55 @@ class Home extends Component {
       </div>
     )
   }
+}
+
+const Features = ({content}) => {
+  if (isEmpty(content))
+    return <Loading />
+
+  const {title, posts} = content.items[0]
+  return (
+    <div>
+      <h3>{title}</h3>
+      <div className='flex-horizontal'>
+      {
+        chunk(posts, 9).map((col, index) =>
+          <div key={`col-${index}`}>
+            <ul style={{listStyleType:'none'}}>
+              {col.map(post =>
+                <li key={post.id} style={{fontWeight:'bold',margin:'20px auto'}}>
+                  <a href={post.meta['hp-link']} style={{lineHeight:'1.5',color:'#000',textDecoration:'none'}}>{post.meta['link-description']}</a>
+                </li>
+              )}
+            </ul>
+          </div>
+        )
+      }
+      </div>
+    </div>
+  )
+}
+
+const Article = ({content}) => {
+  if (isEmpty(content))
+    return <Loading />
+
+  const {title, items} = content
+  const post = items[0].posts[0]
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      <div className='flex-horizontal'>
+        <div style={{background:`linear-gradient(rgba(20,20,20, .5),rgba(20,20,20, .5)),url('${post.image}')`,backgroundPosition:'center center',backgroundSize:'cover',opacity:'.7',minHeight:'280px',flex:1,position:'relative'}}>
+          <h2 style={{color:'#fff',position:'absolute',bottom:0}}>
+            <a href={post.meta.beyondthenumber_link}>{post.title}</a>
+          </h2>
+        </div>
+        <div style={{flex:1}} dangerouslySetInnerHTML={{__html:post.content}}/>
+      </div>
+    </div>
+  )
 }
 
 export default Home
